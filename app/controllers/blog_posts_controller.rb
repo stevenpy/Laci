1f3183d8295dc76b09b8f32f634b59ff3eaf0e2b
stevenpy <stevenphilbert@gmail.com>
2024-08-23 18:57:59 +0200

    Add create method with validation on Post models and generate errors in view

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -12,4 +12,19 @@ class BlogPostsController < ApplicationController
   def new
     @blog_post = BlogPost.new
   end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
 end

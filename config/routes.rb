9a77fde1cb45b01b367d66e517a245b7a6a8aa55
stevenpy <stevenphilbert@gmail.com>
2024-08-26 16:40:29 +0200

    Add routes and edit method to update post + partials

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
 
   # Defines the root path route ("/")
   root "blog_posts#index"

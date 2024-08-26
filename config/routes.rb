9adb9ff223221f1132341b2f81a13bd087eec7d2
stevenpy <stevenphilbert@gmail.com>
2024-08-26 16:59:08 +0200

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  delete "/blog_posts/:id", to: "blog_posts#destroy"
-
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
-
-
+  resources :blog_posts
   # Defines the root path route ("/")
   root "blog_posts#index"
 end

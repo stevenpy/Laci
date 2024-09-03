446440764d37b4303ac2db6c5c0eb79aeac27fbd
stevenpy <stevenphilbert@gmail.com>
2024-09-03 19:09:02 +0200

    rescue pagy error

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,6 +5,8 @@ class BlogPostsController < ApplicationController
   def index
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     @pagy, @blog_posts = pagy(@blog_posts)
+  rescue Pagy::OverflowError
+    redirect_to root_path(page: 1)
   end
 
   def show
